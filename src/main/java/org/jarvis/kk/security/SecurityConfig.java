47b79ff76c53a6a7f79961863358634b07a9a34a
SOOHYUNLIM <sh_7682@naver.com>
2020-01-09 17:37:54 +0900

    Security CORS 추가

--- src/main/java/org/jarvis/kk/security/SecurityConfig.java
+++ src/main/java/org/jarvis/kk/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package org.jarvis.kk.security;
 
 import org.jarvis.kk.service.OAuth2MemberService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,13 +27,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint)
-        .and().headers().frameOptions().disable()
-        .and().authorizeRequests()
-        .antMatchers("/kk/msg", "/kk/lprice","/", "/admin/**").permitAll()
-        .anyRequest().authenticated()
-        .and().logout().logoutSuccessUrl("/kk/dropToken").logoutSuccessHandler(restAuthenticationLogoutSuccessHandler).logoutUrl("/kk/logout").invalidateHttpSession(true).permitAll()
-        .and().oauth2Login().successHandler(restAuthenticationLoginSuccessHandler).userInfoEndpoint().userService(oauth2MemberService);
+        http.csrf().disable().exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and().headers()
+                .frameOptions().disable().and().authorizeRequests()
+                .antMatchers("/kk/msg", "/kk/lprice", "/", "/admin/**").permitAll().anyRequest().authenticated().and()
+                .logout().logoutSuccessUrl("/kk/dropToken").logoutSuccessHandler(restAuthenticationLogoutSuccessHandler)
+                .logoutUrl("/kk/logout").invalidateHttpSession(true).permitAll().and().oauth2Login()
+                .successHandler(restAuthenticationLoginSuccessHandler).userInfoEndpoint()
+                .userService(oauth2MemberService);
     }
-    
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedOrigin("*");
+        // configuration.setAllowedOrigins(Arrays.asList("https://localhost:8080","http://localhost:63343"));
+        // configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
